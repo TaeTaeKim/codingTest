98d2b283af5e16496cb4700dfec41c7ae7a83388
TaeTaeKim <hc07car@gmail.com>
2025-12-31 15:25:53 +0900

    프로그래머스. 해시 - 베스트앨범 문제 해결

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,24 @@ import kotlin.math.abs
 
 
 fun main() {
+    val genres = arrayOf("classic", "pop", "classic", "classic", "pop")
+    val plays = intArrayOf(500, 600, 150, 800, 2500)
+    solution(genres, plays).forEach { println(it) }
 }
 
-fun solution(K: Int, A: Array<IntArray>): Int {
-    return 0
+fun solution(genres: Array<String>, plays: IntArray): IntArray {
+    val songsByGenre = genres.indices
+        .map { Song(it, plays[it], genres[it]) }
+        .groupBy { it.genre }
+        .toList()
+    return songsByGenre
+        .sortedByDescending { (_, songs) -> songs.sumOf { it.playCount } }
+        .flatMap { (_, songs) ->
+            songs.sortedWith(compareByDescending<Song> { it.playCount }.thenBy { it.id })
+                .take(2) // Take top 2
+        }
+        .map { it.id }
+        .toIntArray()
 }
+
+data class Song(val id: Int, val playCount: Int, val genre:String)
