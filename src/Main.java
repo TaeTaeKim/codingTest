5d4294cddc9e5078aae961a76feb759492a0fe3e
김태윤 <mygod456@kakao.com>
2025-12-31 16:43:04 +0900

    프로그래머스. 스택큐-기능개발

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,49 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Main {
-    public void solution() throws Exception {
+    public int[] solution(int[] progresses, int[] speeds) {
+        Queue<Integer> daysQueue = new LinkedList<>();
+        for (int i = 0; i < progresses.length; i++) {
+            int daysNeeded = getLeftDay(progresses[i], speeds[i]);
+            daysQueue.add(daysNeeded);
+        }
 
+        List<Integer> resultList = new ArrayList<>();
+        
+
+        while (!daysQueue.isEmpty()) {
+
+            int deployDay = daysQueue.poll();
+            int featuresDeployed = 1;
+
+
+            while (!daysQueue.isEmpty() && daysQueue.peek() <= deployDay) {
+                daysQueue.poll();
+                featuresDeployed++;
+            }
+            resultList.add(featuresDeployed);
+        }
+
+        return resultList.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int getLeftDay(int progress, int speed) {
+        int remainingProgress = 100 - progress;
+        return (remainingProgress + speed - 1) / speed;
     }
 
     public static void main(String[] args) throws Exception {
-        new Main().solution();
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        int[] solution = new Main().solution(progresses, speeds);
+        // Print the result to the console. Expected: [2, 1]
+        System.out.println("Result: " + Arrays.toString(solution));
+
+        // Example from problem description that would yield [1, 3, 2]
+        int[] progresses2 = {95, 90, 99, 99, 80, 99};
+        int[] speeds2 = {1, 1, 1, 1, 1, 1};
+        int[] solution2 = new Main().solution(progresses2, speeds2);
+        System.out.println("Result 2: " + Arrays.toString(solution2));
     }
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,43 @@
-import kotlin.math.abs
-
-
 fun main() {
+    // Test Case 1: from Main.java
+    val progresses1 = intArrayOf(93, 30, 55)
+    val speeds1 = intArrayOf(1, 30, 5)
+    val solution1 = solution(progresses1, speeds1)
+    println("Result 1: ${solution1.contentToString()} (Expected: [2, 1])")
+
+    println() // Separator
+
+    // Test Case 2: from Main.java
+    val progresses2 = intArrayOf(95, 90, 99, 99, 80, 99)
+    val speeds2 = intArrayOf(1, 1, 1, 1, 1, 1)
+    val solution2 = solution(progresses2, speeds2)
+    println("Result 2: ${solution2.contentToString()} (Expected: [1, 3, 2])")
 }
 
-fun solution(K: Int, A: Array<IntArray>): Int {
-    return 0
+fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+    val leftDays = ArrayDeque<Int>()
+    for (i in progresses.indices) {
+        leftDays.addLast(getLeftDay(progresses[i], speeds[i]))
+    }
+    val deployCounts = mutableListOf<Int>()
+    while(leftDays.isNotEmpty()) {
+        var deployed = 1
+        val needDay = leftDays.removeFirst()
+        while(leftDays.isNotEmpty() && leftDays.first() <= needDay){
+            leftDays.removeFirst()
+            deployed++
+        }
+        deployCounts.add(deployed)
+
+    }
+    return deployCounts.toIntArray()
+}
+
+fun getLeftDay(progress:Int, speed: Int): Int{
+    val leftDays = 100 - progress
+    var toDeploy = leftDays / speed
+    if(leftDays%speed != 0){
+        toDeploy ++
+    }
+    return toDeploy
 }
