b6786954b04a1cdf8778521524278e549f2ec248
김태윤 <mygod456@kakao.com>
2025-12-31 18:15:49 +0900

    프로그래머스. 스택/큐 - 프로세스

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,54 @@ import java.util.*;
 import java.util.stream.*;
 
 public class Main {
-    public void solution() throws Exception {
+    public int solution(int[] priorities, int location) {
+        ArrayDeque<Process> processes = new ArrayDeque<>();
+        for(int i = 0; i < priorities.length; i++){
+            processes.addLast(new Process(i, priorities[i]));
+        }
 
+        int runCount = 0;
+        // location 과 일치하는 값이 나올때
+        while(true){
+            Process max = Collections.max(processes, Comparator.comparing(Process::getPriority));
+            Process first = processes.getFirst();
+            if(first.getPriority() >= max.getPriority()){
+                processes.removeFirst();
+                runCount++;
+                if (first.getId() == location) {
+                    break;
+                }
+            }else{
+                Process poll = processes.poll();
+                processes.addLast(poll);
+            }
+        }
+        System.out.println("runCount = " + runCount);
+        return runCount;
+    }
+    private static class Process{
+        private int id;
+        private int priority;
+
+        public Process(int id, int priority) {
+            this.id = id;
+            this.priority = priority;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
     }
 
+
     public static void main(String[] args) throws Exception {
-        new Main().solution();
+        int[] processes = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+        new Main().solution(processes, location);
     }
 
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,33 @@
-import kotlin.math.abs
-
-
 fun main() {
+    val processes = intArrayOf(1, 1, 9, 1, 1, 1)
+    val location = 0
+    solution(processes, location)
 }
 
-fun solution(K: Int, A: Array<IntArray>): Int {
-    return 0
+fun solution(priorities: IntArray, location: Int): Int {
+    val processQueue = ArrayDeque<Process>()
+    priorities.forEachIndexed { idx, it -> processQueue.addLast(Process(idx, it)) }
+
+    var runCount = 0
+
+    while (true) {
+        val maxPriority = processQueue.maxOf { it.priority }
+        val currentProcess = processQueue.removeFirst()
+
+        if (currentProcess.priority >= maxPriority) {
+            runCount++
+            if (location == currentProcess.id) {
+                break;
+            }
+        } else {
+            processQueue.addLast(currentProcess)
+        }
+    }
+    println("runCount = ${runCount}")
+    return runCount
 }
+
+data class Process(
+    val id: Int,
+    val priority: Int,
+)
